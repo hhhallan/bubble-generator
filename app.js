102f06a911f6839a1e0f850a17a52c6c4d1cc8df
hhhallan <allanstm76@gmail.com>
2021-12-13 12:36:08 +0100

    Bubble Generator

---
+++ app.js
@@ -0,0 +1,43 @@
+const body = document.querySelector('body');
+
+window.addEventListener('click', (e) => {
+    // Create Bubble
+    let bubble = document.createElement('div');
+    bubble.className = 'bubble';
+
+    // Generate random color
+    let color = colorGenerator();
+    bubble.style.backgroundColor = color;
+    body.style.backgroundColor = color;
+
+    // Bubble size
+    let size = bubbleSize();
+    bubble.style.width = size + 'px';
+    bubble.style.height = size + 'px';
+
+    //Bubble Position
+    bubble.style.top = e.y + 'px';
+    bubble.style.left = e.x + 'px';
+
+
+    // Add bubble
+    body.append(bubble);
+
+    // Remove bubble
+    setTimeout(() => {
+        bubble.style.opacity = 0;
+        setTimeout(() => {
+            bubble.remove();
+        }, 1000)
+    }, 2000);
+});
+
+
+// Functions
+const colorGenerator = () => {
+    return '#' + Math.random().toString(16).substr(-6);
+}
+
+const bubbleSize = () => {
+    return Math.round(Math.random() * (100 - 10) + 10);
+}
